41a7960bf52f393a1435ba090aeb4ee3d71541e0
Deepti <deepti_madhuri@yahoo.com>
2020-04-22 15:40:42 -0700

    Done

--- automation_helpers/webdriver.py
+++ automation_helpers/webdriver.py
@@ -6,8 +6,8 @@ class webDriver():
         def loadDriver(self):
                 print("Addig the webdriver details")
                 #driver = webdriver.Firefox("/usr/local/bin/geckodriver")
-                driver = webdriver.Firefox("/usr/local/bin")
-                #self.version = webdriver.Firefox().save_screenshot("screenshot.png")
+                driver = webdriver.Firefox()
+                #self.version = webdriver.Firefox()..save_screenshot("screenshot.png")
                 driver = driver
                 print("Loading the web driver")
                 print("loading the page ...")
